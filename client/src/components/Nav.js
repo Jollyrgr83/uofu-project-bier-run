a911c09b4069f6039790876180e35f34a1881edf
Cade Clark <crazycademan@gmail.com>
2020-08-08 13:01:14 -0600

    Added elements to the main page

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,19 +10,19 @@ import textLogo from "../assets/ppt-logo-text.png";
 function Nav() {
   return (
     <nav className="row local-navbar">
+
       <div className="text-center">
         <img className="text-logo" src={textLogo} alt="Bier Run" />
       </div>
-      <p className="welcome mx-auto">Welcome [username], your address is: 123 Main Street, Anytown, USA, 12345</p>
+      {/* <p className="welcome mx-auto">Welcome [username], your address is: 123 Main Street, Anytown, USA, 12345</p> */}
       <div className="text-center">
         <img className="logo" src={logo} alt="Barry" />
       </div>
-      <p className="welcome mx-auto">You have no active orders</p>
+      {/* <p className="welcome mx-auto">You have no active orders</p> */}
       <div className="svg-container text-center">
         <FontAwesomeIcon icon={faCog} className="nav-icons" />
         <FontAwesomeIcon icon={faSignOutAlt} className="nav-icons" />
       </div>
-
 
     </nav>
   );
--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,21 +1,50 @@
 import React from 'react';
-// import './HomePage.css';
-// import logo from '../assets/images/BierRunLogo.svg';
+import "../HomePage/HomePage.css"
+import logo from "../../assets/images/BierRunLogoNoBG.svg"
 
 function HomePage() {
   return (
     <main>
       <div class="topnav">
         <div class="topnav-centered">
-          <a href="#home" class="active"><img id="center-logo" alt="logo" src="" /></a>
+          <a href="#home" class="active"><img id="center-logo" alt="logo" src={logo} /></a>
         </div>
       </div>
 
       <div class="about-us">
-          Simple About Us
+
+        <div class="content">
+
+          <div class="about-title">
+            <h1>How does it work?</h1>
+          </div>
+
+          <ul class="columns-3">
+            <li>
+              <h2>Login</h2>
+              <p>Login to your account. If you dont have an account you can register for free. Its as easy as clicking just a couple buttons. You can also log in with Google, if you want to make it that much easier.</p>
+            </li>
+
+            <li>
+              <h2>Order</h2>
+              <p>Scroll through our many beer options and pick your favorite! You can choose from a variety of common beers found at most retail locations. You can choose if you only want a couple for you or enough for an army. The choice is yours.</p>
+            </li>
+
+            <li>
+              <h2>Wait</h2>
+              <p>Sit back and relax while one of our drivers picks up your order and delivers it straight to your door.</p>
+            </li>
+          </ul>
+
+          {/* <div class="test-div"><h1>Test Div</h1></div> */}
+        </div>
+
       </div>
+
+
+
     </main>
-  ) 
+  )
 }
 
 export default HomePage;
