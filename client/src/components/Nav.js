8059f42f4ae2ebedde5289157b0a1e2be70cee3a
jollyrgr83 <jasongmangin@gmail.com>
2020-08-10 14:01:21 -0600

    added customer page and css

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-import LoginButton from "./LoginButton";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-import { faDoorClosed } from "@fortawesome/free-solid-svg-icons";
 
 import logo from "../assets/ppt-logo.png";
-import textLogo from "../assets/ppt-logo-text.png";
 
 function Nav() {
   function AlertMsg(e) {
@@ -16,47 +12,49 @@ function Nav() {
   }
 
   return (
-    <div>
-    <nav className="row local-navbar">
-      <div className="text-center nav-left">
-        {/* <img className="text-logo" src={textLogo} alt="Bier Run" /> */}
-        <div className="text-logo-container">
-          <div className="text-logo-top">BIER</div>
-          <div className="text-logo-bottom">RUN</div>
+    <div className="temp-container">
+      <ul>
+        <li>
+          <a href="/">Home (/)</a>
+        </li>
+        <li>
+          <a href="/auth/login">Login (/auth/login)</a>
+        </li>
+        <li>
+          <a href="/home">Login Page (/home)</a>
+        </li>
+        <li>
+          <a href="/driver">Driver (/driver)</a>
+        </li>
+        <li>
+          <a href="/customer">Customer (/customer)</a>
+        </li>
+      </ul>
+      <nav className="row local-navbar">
+        <div className="text-center nav-left">
+          <div className="text-logo-container">
+            <div className="text-logo-top">BIER</div>
+            <div className="text-logo-bottom">RUN</div>
+          </div>
         </div>
-      </div>
-      <div className="text-center nav-center">
-        <img className="logo" src={logo} alt="Barry" />
-      </div>
-      <div className="svg-container text-center">
-        <FontAwesomeIcon icon={faCog} onClick={AlertMsg} className="nav-icons" id="account-settings" />
-        <FontAwesomeIcon icon={faSignOutAlt} onClick={AlertMsg} className="nav-icons" id="logout" />
-        {/* <FontAwesomeIcon icon={faDoorClosed} onClick={AlertMsg} className="nav-icons" /> */}
-      </div>
-
-
-    </nav>
-    <div className="row message-container">
-      <p className="welcome mx-auto">Welcome [username], your address is: 123 Main Street, Anytown, USA, 12345</p>
-      <p className="welcome mx-auto">You have no active orders</p>
-    </div>
-    <ul>
-      <li>
-        <a href="/">Home (/)</a>
-      </li>
-      <li>
-        <a href="/auth/login">Login (/auth/login)</a>
-      </li>
-      <li>
-        <a href="/home">Login Page (/home)</a>
-      </li>
-      <li>
-        <a href="/driver">Driver (/driver)</a>
-      </li>
-    </ul>
-    <div className="row">
-      <LoginButton />
-    </div>
+        <div className="text-center nav-center">
+          <img className="logo" src={logo} alt="Barry" />
+        </div>
+        <div className="svg-container text-center">
+          <FontAwesomeIcon
+            icon={faCog}
+            onClick={AlertMsg}
+            className="nav-icons"
+            id="account-settings"
+          />
+          <FontAwesomeIcon
+            icon={faSignOutAlt}
+            onClick={AlertMsg}
+            className="nav-icons"
+            id="logout"
+          />
+        </div>
+      </nav>
     </div>
   );
 }
--- client/src/pages/DriverPage/index.js
+++ client/src/pages/DriverPage/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./index.css";
+
 const dummyDrivers = [
   {
     id: 1,
