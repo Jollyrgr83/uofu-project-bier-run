6f258564bd91421cb4d73f3adb1628af28b5ee37
Cade Clark <crazycademan@gmail.com>
2020-08-10 21:19:35 -0600

    Finished home page. Still needs to be responsive

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -5,38 +5,57 @@ import logo from "../../assets/images/BierRunLogoNoBG.svg"
 function HomePage() {
   return (
     <main>
-      <div class="topnav">
-        <div class="topnav-centered">
-          <a href="#home" class="active"><img id="center-logo" alt="logo" src={logo} /></a>
+      <div className="topnav">
+        <div className="topnav-centered">
+          <a href="#home" className="active"><img id="center-logo" alt="logo" src={logo} /></a>
         </div>
       </div>
 
-      <div class="about-us">
+      <div className="about-us">
 
-        <div class="content">
+        <div className="content">
 
-          <div class="about-title">
+          <div className="about-title">
             <h1>How does it work?</h1>
           </div>
 
-          <ul class="columns-3">
-            <li>
-              <h2>Login</h2>
-              <p>Login to your account. If you dont have an account you can register for free. Its as easy as clicking just a couple buttons. You can also log in with Google, if you want to make it that much easier.</p>
-            </li>
+          <div className="container-fluid">
+            <div className="row abouts">
 
-            <li>
-              <h2>Order</h2>
-              <p>Scroll through our many beer options and pick your favorite! You can choose from a variety of common beers found at most retail locations. You can choose if you only want a couple for you or enough for an army. The choice is yours.</p>
-            </li>
+              <div className="col-sm-4">
+                <h2>Login</h2>
+                <p>Login to your account. If you dont have an account you can register for free. Its as easy as clicking just a couple buttons. You can also log in with Google, if you want to make it that much easier.</p>
+              </div>
 
-            <li>
-              <h2>Wait</h2>
-              <p>Sit back and relax while one of our drivers picks up your order and delivers it straight to your door.</p>
-            </li>
-          </ul>
+              <div className="col-sm-4">
+                <h2>Order</h2>
+                <p>Scroll through our many beer options and pick your favorite! You can choose from a variety of common beers found at most retail locations. You can choose if you only want a couple for you or enough for an army. The choice is yours.</p>
+              </div>
 
-          <div class="test-div"><h1>Test Div</h1></div>
+              <div className="col-sm-4">
+                <h2>Relax</h2>
+                <p>Sit back and relax while one of our drivers picks up your order and delivers it straight to your door.</p>
+              </div>
+            </div>
+          </div>
+
+          <hr className="break-1" />
+
+          <div className="container">
+            <div className="row">
+
+              <div className="col-md-4"></div>
+
+              <div className="col-md-4">
+                <form method="get" action="/customer">
+                  <button className="begin-button" onclick="">Get Started</button>
+                </form>
+              </div>
+              
+
+              <div className="col-md-4"></div>
+            </div>
+          </div>
         </div>
 
       </div>
