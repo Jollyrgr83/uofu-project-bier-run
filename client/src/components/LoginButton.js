be592e59963b95f02d1432fb966b7d017287b5ad
jollyrgr83 <jasongmangin@gmail.com>
2020-08-11 18:55:16 -0600

    resolved package.json merge conflict

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+const LoginButton = ({ classNameInput, buttonTextInput }) => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className={classNameInput} onClick={() => loginWithRedirect()}>{buttonTextInput}</button>;
 };
 
 export default LoginButton;
--- client/src/components/NewNav/NewNav.js
+++ client/src/components/NewNav/NewNav.js
@@ -1,29 +1,45 @@
 import React from "react"
 import "./NewNav.css"
+import LoginLink from "../LoginLink";
+import logo from "../../assets/images/"
 
 function NewNav() {
     return (
         <mainnav>
-
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <a className="navbar-brand" href="#">Bier Run</a>
+            <nav className="navbar navbar-light local-nav-font">
+            {/* <nav className="navbar navbar-expand-xl navbar-light local-nav-font"> */}
+                <a className="navbar-brand text-logo" href="/"><span className="text-bier">Bier</span><span className="text-run">Run</span></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav mr-auto">
+                  <div className="col-sm-6">
+                    <ul class="navbar-nav text-end">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="/features">Features</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="/pricing">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/driver">Driver Page (testing)</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/customer">Customer Page (testing)</a>
+                        </li>
+                        <li className="nav-item">
+                          <LoginLink />
                         </li>
                     </ul>
-                    <button className="login-button" type="submit">Login</button>
+                    {/* <button className="login-button" type="submit">Login</button> */}
+                  </div>
+                  <div className="col-sm-6">
+
+                  </div>
                 </div>
             </nav>
 
