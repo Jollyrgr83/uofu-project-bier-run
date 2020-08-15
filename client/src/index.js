6f86ce336dbdef5e6d7aa109a0fd6577a05ace2f
jollyrgr83 <jasongmangin@gmail.com>
2020-08-15 11:23:39 -0600

    deploying to heroku v2

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,14 @@ import App from "./App.js";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const localURL = "http://localhost:3000/customer";
+const herokuURL = "https://bier-run.herokuapp.com/customer";
 
 ReactDOM.render(
   <Auth0Provider
     domain="dev-w193otkg.us.auth0.com"
     clientId="XtMtHNFaHul68C8mV9F2ozyrdaCOpw7x"
-    redirectUri="http://localhost:3000/customer"
+    redirectUri={herokuURL}
     audience="https://dev-w193otkg.us.auth0.com/api/v2/"
     scope="read:current_user update:current_user_metadata"
   >
